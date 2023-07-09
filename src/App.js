2b0cf5af1b1e1f1641ac8913efa3de81b090277c
prasanna3511 <80350696+prasanna3511@users.noreply.github.com>
2023-07-10 01:44:34 +0530

    stockist added and billing page updated

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Dashboard from './Pages/Dashboard/Dashboard';
 import Inventory from './Pages/Management/Inventory';
 import Retail from './Pages/Management/Retail';
 import Bill from '../src/components/bill/Bill'
+import Stockist from './Pages/Management/Stockist'
 
 function App() {
   return (
@@ -15,6 +16,8 @@ function App() {
     <Route path="/inventory" element={  <Inventory/>} />
     <Route path="/retail" element={  <Retail/>} />
     <Route path="/bill" element={  <Bill/>} />
+    <Route path="/stockist" element={  <Stockist/>} />
+
 
     </Routes>
     </BrowserRouter>
